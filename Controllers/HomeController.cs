327988de3a1c9b1376e215cfa0391557a6640d0b
GRAPECITY\Dushyant.Sharma <dushyant.sharma@grapecity.com>
2021-09-14 15:11:33 +0530

    second commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using C1Control.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace C1Control.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(Country.GetData(10));
+        }
+    }
+}
---
+++ Models/Country.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace C1Control.Models
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string CountryName { get; set; }
+        public int Downloads { get; set; }
+        public int Sales { get; set; }
+
+        public int Amount { get; set; }
+
+        public int Extra { get; set; }
+
+        private static List<string> CountryNames = new List<string>
+        { "United States", "United Kingdom", "Canada", "Japan", "China", "France", "German", "Italy", "Korea", "Australia" };
+
+        public static List<Country> GetData(int total)
+        {
+            var rand = new Random(0);
+            var list = Enumerable.Range(0, total).Select(i =>
+            {
+                string CntryName = CountryNames[rand.Next(0, CountryNames.Count - 1)];
+                int download = rand.Next();
+                int sales = rand.Next(0, 1000);
+                int amt = rand.Next(1000, 10000);
+                int extra = rand.Next(0, 5000);
+
+                return new Country
+                {
+                    Id = i + 1,
+                    CountryName = CntryName,
+                    Downloads = download,
+                    Sales = sales,
+                    Amount = amt,
+                    Extra = extra
+                };
+            });
+            return list.ToList();
+        }
+    }
+}
